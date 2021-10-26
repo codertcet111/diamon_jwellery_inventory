3f096a2783ec867b7c285d8547b460750b5ec018
Swapnil Kumawat <swapnil.kumawat@zingoy.com>
2021-10-27 01:29:24 +0530

    changes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module AggrawalSalesPurchase
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.require_master_key = false
+    config.secret_key_base = 'blipblapblup'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
